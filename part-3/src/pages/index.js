fe6a58dc0d4b8f8dba8019c107cf57a84425b661
Pierre Berchtold <pierre.berchtold@gmail.com>
2018-03-29 10:57:20 +0200

    [Part 3] Introduce Cockpit Layout in pages/index.js

--- part-3/src/pages/index.js
+++ part-3/src/pages/index.js
@@ -1,33 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Img from "gatsby-image";
+import Img from 'gatsby-image';
+
+import CockpitLayout from '../components/CockpitLayout';
 
 const IndexPage = ({data}) => {
   const { page, projects } = data;
 
   return (
     <div>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: page.content,
-        }}
-      />
-      <ul className="project-grid">
-        {projects.edges.map(({ node }, index) => (
-          <li key={node.slug}  className={`project-item project-${index%3 + 1}`}>
-            <Link to={`project/${node.slug}`} className="project-item__inner">
-              <Img sizes={node.preview.localFile.childImageSharp.sizes} className="project-item__image" />
-              <h3 className="project-item__title">{node.title}</h3>
-            </Link>
-          </li>
-        ))} 
-      </ul>
-      <div className="card">
-        <p>We love to do stuff that make people happy.</p>
-        <Link to="/about" className="button">
-          More about us
-        </Link>
-      </div>      
+      <CockpitLayout
+          layout={page.content_parsed}
+          data={{ projects }}
+        />      
     </div>
   )
 }
